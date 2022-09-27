06b81a681c85ec31628b3e7e2674aa9b6abfae1c
HJK <huangjklnu@foxmail.com>
2022-09-27 10:58:54 +0800

    初始化项目

---
+++ src/main/resources/static/uniapp/pages/square/square.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		广场
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
