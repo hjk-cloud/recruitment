d2c5430beb2de22e7f5090dd582855869dcb29ca
HJK <huangjklnu@foxmail.com>
2022-09-28 16:45:25 +0800

    初始化项目

---
+++ src/main/java/edu/lnu/recruitment/admin/entity/Admin.java
@@ -0,0 +1,18 @@
+package edu.lnu.recruitment.admin.entity;
+
+import lombok.Data;
+
+/**
+ * @Package: edu.lnu.recruitment.admin.entity
+ * @ClassName: Admin
+ * @Author: huangjk
+ * @CreateTime: 2022/9/28 15:54
+ * @Description:
+ */
+@Data
+public class Admin {
+    private int id;
+    private String name;
+    private String password;
+
+}
